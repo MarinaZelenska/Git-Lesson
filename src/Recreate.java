a2e74df6499923b66a1226c28c8adda50fa4b3af
Marina Zelenskaya <osychenkomarina@gmail.com>
2020-09-30 22:03:07 +0300

    Test reset

--- src/Recreate.java
+++ src/Recreate.java
@@ -1,7 +1,14 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recreate {
     public static void main(String[] args) {
+        List<Integer> student = new ArrayList<>();
+        student.add(1);
+        student.add(2);
+        System.out.println(student);
         System.out.println("Create new class");
     }
 }
