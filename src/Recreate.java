fe8bbac1e993128d3b088c1a49d5459ce9b8c22f
Marina Zelenskaya <osychenkomarina@gmail.com>
2020-09-30 20:16:18 +0300

    Add new class Recreate and delete task4

---
+++ src/Recreate.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Recreate {
+    public static void main(String[] args) {
+        System.out.println("Create new class");
+    }
+}
--- src/Task4.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public class Task4 {
-    public static void main(String[] args) {
-        System.out.println("Learn git");
-    }
-}
